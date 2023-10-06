3999b682f2d30cbae8ad9c1037a82546dba19db0
ahmad rangga <96713161+RanggaKu@users.noreply.github.com>
2023-10-06 18:30:56 +0700

    nothing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,7 @@ export default {
   ],
   theme: {
     extend: {},
-    container:{center:true},
-      screens: {
-        'sm': '576px',
-        // => @media (min-width: 576px) { ... }
-  
-        'md': '960px',
-        // => @media (min-width: 960px) { ... }
-  
-        'lg': '1440px',
-        // => @media (min-width: 1440px) { ... }
-      },
+    container:{center:true}
   },
   plugins: [],
 }
